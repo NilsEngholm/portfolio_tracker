cb311c0428c4dc200bdf8612109cb69b791f8f78
NilsEngholm <Nils.engholm@gmail.com>
2024-10-01 16:47:53 -0600

    changed from conda to gitignore

--- pull_yfinance.py
+++ pull_yfinance.py
@@ -3,3 +3,22 @@ import yfinance as yf
 import pandas as pd
 import requests
 
+with open('template.yaml') as stonks:
+    try:
+        cfg = yaml.safe_load(stonks)
+        print('Config file loaded successfully')
+    except yaml.YAMLError as exc:
+        print(exc)
+
+#load portfolio data from config file
+portfolio = cfg['portfolio']
+
+#list all ticker symbols in portfolio
+def getSymbols(portfolio):
+    symbols = []
+    for holding in portfolio:
+        symbols.append(holding)
+    return symbols
+
+tickerList = getSymbols(portfolio)
+print(tickerList)
